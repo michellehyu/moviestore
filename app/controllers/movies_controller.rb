65cf78a13bdca5985e03edf9d65c4c4174d2da76
Michelle Yu <michellehyu7@gmail.com>
2015-05-26 23:01:13 -0700

    finish tutorial

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,17 @@
+class MoviesController < ApplicationController
+  before_action :set_movie, only: [:show, :edit, :update, :destroy]
+
+  # GET /movies
+  # GET /movies.json
+  def index
+    @movies = Movie.all
+  end
+
+  # GET /movies/1
+  # GET /movies/1.json
+  def show
+    @movie = Movie.find(params[:id])
+    @cart_action = @movie.cart_action current_user.try :id
+  end
+
+end
